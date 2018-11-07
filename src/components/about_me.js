17b88876bc2a45c8bc402938840f7398ef0f45b2
NathanielYoung <young.nathaniel95@gmail.com>
2018-11-06 19:54:24 -0700

    About Me

--- src/components/about_me.js
+++ src/components/about_me.js
@@ -57,7 +57,7 @@ export default class About extends Component {
                                 <img src = "\assets\ES6LOGO.svg"></img> JavaScript
                             </div>
                             <a>
-                                With JavaScript, iv spent about 6+ Weeks learning, building stuff on repl.it, CodePen, Visual Studio Code making the Basic Html CSS Websites, and React / Redux.
+                                With JavaScript, I've spent about 6+ Weeks learning, building stuff on repl.it, CodePen, Visual Studio Code making the Basic Html CSS Websites, and React / Redux.
                             </a>
                             <div>
                                 <img src = "\assets\PythonLogo.png"></img> Python
@@ -70,6 +70,31 @@ export default class About extends Component {
                             </div>
                             <a>
                                 I learned C Sharp in HighSchool and is the first full language I learned, I used it mostly to build games in Unity. I learned C++ during my Second year of Computer Programming.  it was more of a game code but Unity can only accept C#.
+                            </a>
+                            <div>
+                                <img src = "\assets\mysql.png"></img> MySQL
+                            </div>
+                            <a>
+                                I know how to build with MySQL with Python API's but im learning how to work with it in React.js / React Native.
+                            </a>
+                            <div>
+                                <img src ="\assets\HTML.png"></img> HTML | <img src ="\assets\CSS.png"></img> CSS | <img src ="\assets\SCSS.png"></img> SCSS
+                            </div>
+                            <a>
+                                These Website languages is what I used to style and order this entire website, I use the SCSS style more than SASS but mostly just becasue its the newest version. HTML and CSS are pretty much straight forward its just align than style with CSS / SCSS, although I am still learning alot with CSS especially animations.
+                            </a>
+                            <div>
+                                <img src ="\assets\PowerShell.png"></img> Shell
+                            </div>
+                            <a>
+                                Shell is the Terminal or/ Command Prompt for either Mac or Windows, I know the basic's of navigating and calling programs, using git and setting up scripts with in the terminal.
+                            </a>
+                            <div>
+                                <img src ="\assets\React1.png"></img> React | <img src ="\assets\redux.png"></img> Redux
+                            </div>
+                            <a>
+                                React is what allows websites to be needed loaded once so its smooth transition between pages. With React I know how to manage the State and the this statement while within the class components.
+                                Redux is an addition to React to allow everything to be put into the store and distriputed to all files allowing seemless data transfer instead of importing.
                             </a>
                         </div>
                         <Link to="/contact" id = "ContactMe">
